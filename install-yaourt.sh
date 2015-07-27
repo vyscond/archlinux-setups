32806f0bd9f5c9e26ad5ed80709a16108ce05c8f
Vyscond <vyscond@users.noreply.github.com>
2015-07-27 13:04:10 -0300

    Update install-yaourt.sh

--- install-yaourt.sh
+++ install-yaourt.sh
@@ -1,2 +1,2 @@
-echo -e "[archlinuxfr]\n\nSigLevel = Never\nServer = http://repo.archlinux.fr/$arch\n" >> /etc/pacman.conf
+echo -e "[archlinuxfr]\nSigLevel = Never\nServer = http://repo.archlinux.fr/\$arch\n" >> /etc/pacman.conf
 pacman -Sy yaourt --noconfirm
